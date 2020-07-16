67a44e40c3e711a9930665e1505fcfe3105a777c
mayurkoli128 <kolimayur566@gmail.com>
2020-07-16 14:31:42 +0530

    build seg tree in linear time

--- Segment tree/segment_tree.cpp
+++ Segment tree/segment_tree.cpp
@@ -68,19 +68,51 @@ struct SegTree {
         traverse(2*index + 1);
         traverse(2*index + 2);
     }
+    void build(vector<int>& vec, int index, int lx, int ly, int size) {
+
+        if(lx - ly == 0 &&  size > lx){
+            segment[index] = vec[lx];
+            return;
+        }
+
+        int mid = lx - (lx - ly) / 2;
+
+        build(vec, index*2 + 1, lx, mid, size);
+        build(vec, index*2 + 2, mid + 1, ly, size);
+
+        segment[index] = segment[index*2 + 1] + segment[index*2 + 2];
+
+        return;
+    }
+    void build(vector<int>& vec) {
+        build(vec, 0, 0, size - 1, vec.size());        
+    
+    }
 
 };
 int main() {
     int N = 8;
-    vector<int> vec = {3, 1, 2, 5, 6, 4, 3, 2};
+    cin >> N;
+    vector<int> vec(N); //= {3, 1, 2, 5, 6, 4, 3, 2};
     //vector<int> vec = {-1, 3, 4, 0, 2, 1};
-    N = vec.size();
 
     SegTree tree(N);
 
-    for(int i = 0; i < N; ++i) {
-        tree.update(i, vec[i]);
+    // build segment tree in NlogN time complexity...
+    // for(int i = 0; i < N; ++i) {
+    //     tree.update(i, vec[i]);
+    // }
+
+
+    // build segment tree in linear N time....
+
+    for(int i = 0 ; i < N; ++i) {
+        int in;
+        cin >> in;
+        vec[i] = in;
     }
+
+    tree.build(vec);
 
     // queries...
     vector<pair<int, int>> queries = {{0, 1}, {0, 7}, {3, 3}, {4, 4}, {1, 4}, {1, 6}, {5, 7}, {1, 7}, {1, 7}, {2, 7}, {3, 7}, {4, 5}};
@@ -90,6 +122,6 @@ int main() {
 
         cout << "( " << p.first << " ," << p.second << " ) : " << tree.get(p.first, p.second) << "\n"; 
     }
-    //tree.traverse();
+    tree.traverse();
     return 0;
 }
